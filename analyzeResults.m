c42147c670b74685b7cfbefdc87937973e576618
greemote <65730530+greemote@users.noreply.github.com>
2022-06-11 21:50:55 +0200

    Add files via upload

---
+++ analyzeResults.m
@@ -0,0 +1,11 @@
+% AGH UST WFiIS
+% metody inteligencji obliczeniowej
+% temat: 8 - Predykcja zainteresowania postami w social media z użyciem metod NLP 
+% grupa: Arkadiusz Trojanowski, Łukasz Kisielewski, Wiktor Gaworek
+
+
+%%
+
+clc;
+
+Accuracy = 100. - (abs(Difference ./ dataTest(:, idx0 + 6))) * 100.;
