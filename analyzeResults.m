38c04612f633adb93dfabc98ce2d171a3e3701d8
greemote <65730530+greemote@users.noreply.github.com>
2022-06-12 17:41:28 +0200

    Add files via upload

--- analyzeResults.m
+++ analyzeResults.m
@@ -10,3 +10,17 @@ clc;
 
 Accuracy = 100. - (abs(Difference ./ dataTest(:, idx0 + 6))) * 100.;
 mean(Accuracy)
+
+mostPopularWords1 = """" + mostPopularWords + """";
+
+variableNames = ["date" "sentiment" "length" "mentions" "hashtags" "retweets"];
+tbl1 = array2table(dataTest(:, 1 : idx0), 'VariableNames', mostPopularWords1);
+tbl2 = table(dataTest(:, idx0 + 1), dataTest(:, idx0 + 2), dataTest(:, idx0 + 3), dataTest(:, idx0 + 4), dataTest(:, idx0 + 5), dataTest(:, idx0 + 6), 'VariableNames', variableNames);
+tbl = [tbl1 tbl2];
+rng('default');
+mdl = fitrkernel(tbl, 'retweets', 'CategoricalPredictors', 'all');
+
+queryPoint = tbl(8469, :);
+explainer = shapley(mdl, tbl, 'QueryPoint', queryPoint, 'UseParallel', true);
+explainer.ShapleyValues
+plot(explainer);
